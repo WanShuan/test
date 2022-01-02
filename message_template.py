be558419af3432f2e4d9d0e2ea9fe07bf19d7377
WanShuan <96587400+WanShuan@users.noreply.github.com>
2022-01-02 19:09:42 +0800

    Add files via upload

---
+++ message_template.py
@@ -0,0 +1,346 @@
+
+main_menu = {
+  "type": "carousel",
+  "contents": [
+    {
+      "type": "bubble",
+      "size": "micro",
+      "hero": {
+        "type": "image",
+        "size": "full",
+        "aspectMode": "fit",
+        "aspectRatio": "1.25:1",
+        "margin": "none",
+        "url": "https://bestdesign.com.tw/wp-content/uploads/2015/05/icon_project_04.png"
+      },
+      "footer": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "label": "我要訂位",
+              "text": "我要訂位"
+            },
+            "margin": "lg",
+            "style": "primary",
+            "height": "md"
+          }
+        ]
+      }
+    },
+    {
+      "type": "bubble",
+      "size": "micro",
+      "hero": {
+        "type": "image",
+        "url": "https://pic03.eapple.com.tw/shineworld11/sw_img57.png",
+        "size": "full",
+        "aspectMode": "cover",
+        "aspectRatio": "1.25:1"
+      },
+      "footer": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "button",
+            "action": {
+              "type": "message",
+              "label": "美味菜單",
+              "text": "美味菜單"
+            },
+            "margin": "lg",
+            "height": "md",
+            "style": "primary"
+          }
+        ]
+      }
+    }
+  ]
+}
+
+menu = {
+  "type": "bubble",
+  "header": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "小店菜單",
+        "size": "xl",
+        "weight": "bold",
+        "align": "center",
+        "decoration": "underline",
+        "offsetTop": "xxl",
+        "offsetBottom": "xs"
+      }
+    ]
+  },
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "box",
+        "layout": "baseline",
+        "contents": [
+          {
+            "type": "text",
+            "text": "控肉飯",
+            "contents": [],
+            "align": "end",
+            "size": "lg"
+          },
+          {
+            "type": "text",
+            "text": "65元",
+            "align": "center",
+            "size": "lg"
+          },
+          {
+            "type": "text",
+            "text": "參考圖片",
+            "action": {
+              "type": "message",
+              "label": "action",
+              "text": "控肉飯"
+            },
+            "align": "end"
+          }
+        ]
+      },
+      {
+        "type": "box",
+        "layout": "baseline",
+        "contents": [
+          {
+            "type": "text",
+            "text": "虱目魚飯",
+            "align": "end",
+            "size": "lg"
+          },
+          {
+            "type": "text",
+            "text": "65元",
+            "align": "center",
+            "size": "lg"
+          },
+          {
+            "type": "text",
+            "text": "參考圖片",
+            "align": "end",
+            "action": {
+              "type": "message",
+              "label": "action",
+              "text": "虱目魚飯"
+            }
+          }
+        ]
+      },
+      {
+        "type": "box",
+        "layout": "baseline",
+        "contents": [
+          {
+            "type": "text",
+            "text": "排骨飯",
+            "align": "end",
+            "size": "lg"
+          },
+          {
+            "type": "text",
+            "text": "70元",
+            "align": "center",
+            "size": "lg"
+          },
+          {
+            "type": "text",
+            "text": "參考圖片",
+            "align": "end",
+            "action": {
+              "type": "message",
+              "label": "action",
+              "text": "排骨飯"
+            }
+          }
+        ]
+      },
+      {
+        "type": "box",
+        "layout": "baseline",
+        "contents": [
+          {
+            "type": "text",
+            "text": "雞腿飯",
+            "align": "end",
+            "size": "lg"
+          },
+          {
+            "type": "text",
+            "text": "70元",
+            "align": "center",
+            "size": "lg"
+          },
+          {
+            "type": "text",
+            "text": "參考圖片",
+            "align": "end",
+            "action": {
+              "type": "message",
+              "label": "action",
+              "text": "雞腿飯"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  "footer": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "button",
+        "action": {
+          "type": "message",
+          "label": "返回主選單",
+          "text": "主選單"
+        },
+        "style": "primary"
+      }
+    ]
+  }
+}
+
+information = {
+  "type": "bubble",
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "歡迎光臨小店  !!!",
+        "offsetBottom": "md"
+      },
+      {
+        "type": "box",
+        "layout": "baseline",
+        "contents": [
+          {
+            "type": "icon",
+            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+          },
+          {
+            "type": "text",
+            "text": "您需要回答5個問題"
+          }
+        ]
+      },
+      {
+        "type": "text",
+        "text": "貴姓、電話號碼、人數、日期、時間",
+        "offsetStart": "xl"
+      },
+      {
+        "type": "box",
+        "layout": "baseline",
+        "contents": [
+          {
+            "type": "icon",
+            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+          },
+          {
+            "type": "text",
+            "text": "如果需要重新填寫"
+          }
+        ]
+      },
+      {
+        "type": "text",
+        "text": "請回答\"重新填寫\"",
+        "offsetStart": "xl"
+      },
+      {
+        "type": "box",
+        "layout": "baseline",
+        "contents": [
+          {
+            "type": "icon",
+            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
+          },
+          {
+            "type": "text",
+            "text": "如果需要返回主選單"
+          }
+        ]
+      },
+      {
+        "type": "text",
+        "text": "請回答\"主選單\"",
+        "offsetStart": "xl"
+      }
+    ]
+  },
+  "footer": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "button",
+        "action": {
+          "type": "message",
+          "label": "開始填寫",
+          "text": "開始填寫"
+        },
+        "style": "primary",
+        "offsetBottom": "md"
+      },
+      {
+        "type": "button",
+        "action": {
+          "type": "message",
+          "label": "返回主選單",
+          "text": "主選單"
+        },
+        "style": "primary"
+      }
+    ]
+  }
+}
+
+reservation_success_information = {
+  "type": "bubble",
+  "header": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": "訂位成功!",
+        "size": "xxl",
+        "align": "center",
+        "offsetTop": "xl"
+      }
+    ]
+  },
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "button",
+        "action": {
+          "type": "message",
+          "label": "返回主選單",
+          "text": "主選單"
+        },
+        "style": "primary"
+      }
+    ]
+  }
+}
